660f18559a277039b0cdcdd448c6582e935929d1
Saber Seddik <seddik.saber@gmail.com>
2021-12-27 13:50:32 -0500

    Add appinsight

--- app/src/main/java/com/saber/controller/MemoryLeak.java
+++ app/src/main/java/com/saber/controller/MemoryLeak.java
@@ -1,5 +1,7 @@
 package com.saber.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,15 @@ import java.util.List;
 
 @RestController
 public class MemoryLeak {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemoryLeak.class);
     private static List<String> container = new ArrayList<>();
 
     @PostMapping("/addString")
     public void addString(@RequestParam(name = "value") String value) {
-        container.add(value);
+        try {
+            container.add(value);
+        } catch (Throwable t) {
+            LOGGER.error("Failed with out of memory.", t);
+        }
     }
 }
